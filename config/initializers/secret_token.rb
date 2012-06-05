0d856c51811320d18bd90f9d419332db54e353c9
Nikita Levchuk <nikitalevchuk@gmail.com>
2012-06-05 11:08:02 +0700

    added .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arvsmongo::Application.config.secret_token = '2650fec99f9aca6c793f45a41240390a1da366ac7877126cbc4aad11ee3da2c8caab2f3e97ec6726052304979c3d7543781b9a3c9e627bf02b1411b94af1be66'
